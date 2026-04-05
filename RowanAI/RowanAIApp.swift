3db2b37b2e0881fb52d16a8b84cecec377322996
Charles Rakita <rakitastudios@gmail.com>
2026-04-05 15:23:40 -0400

    Initial Commit

---
+++ RowanAI/RowanAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RowanAIApp.swift
+//  RowanAI
+//
+//  Created by Chaz Rakita on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct RowanAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
